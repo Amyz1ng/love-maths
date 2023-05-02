c38bbeb8f20a85f4647e8c3ec3b994b705846129
Amy - Leigh Sawyer <amyleighsawyer97@gmail.com>
2023-05-02 11:04:10 +0100

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function rungame(){
+
+}
+
+function checkanser(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditonQuestion(){
+
+}
+
+function displaySubtractionQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
